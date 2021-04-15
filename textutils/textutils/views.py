43c64a5190ae020a7c48ce0ea0f26d3c49c11a77
gopijaiswal0607 <gopi.jais0607@gmail.com>
2021-04-15 20:22:36 +0530

    This is commit

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -15,21 +15,63 @@ def analyze(request):
     #get the text
     djtext=request.GET.get('text','default')
     removepuch=request.GET.get('removepunc','off') # make default = off
+    capital= request.GET.get('capitalize','off')
+    newlineremove = request.GET.get('newlineremover','off')
+    extraspaceremove = request.GET.get('extraspaceremover','off')
+
     print(removepuch)
     print(djtext)
     # analyze the text
     analyzed= ""
+    atext=djtext
+    flag=0
+    message=""
     punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     if removepuch == "on":
-
-        for char in djtext:
+        message+='Removed Punctuations,'
+        flag=1
+        analyzed= ""
+        for char in atext:
             if char not in punctuations:
                 analyzed = analyzed+char
-        params={'purpose':'Removed Punctuations','analyzed_text':analyzed}
-        return render(request,'analyze.html',params)
+        atext=analyzed
+        #params={'purpose':'Removed Punctuations','analyzed_text':analyzed}
+        #return render(request,'analyze.html',params)
     #return HttpResponse("remove puncuation")
+    if capital == 'on':
+        message+='Capitalized form ,'
+        flag=1
+        analyzed= ""
+        for char in atext:
+            if char in punctuations:
+                analyzed+=char
+            else:
+                analyzed+=char.upper()
+        atext=analyzed
+        #params={'purpose':'Capitalized form','analyzed_text':analyzed}
+        #return render(request,'analyze.html',params)
+    if newlineremove == 'on':
+        flag=1
+        message+= 'New line removed'
+        analyzed=""
+        for char in atext:
+            if char != '\n':
+                analyzed+= char
+        atext = analyzed
+    if extraspaceremove == 'on':
+        flag=1
+        message+= 'Extra space removed'
+        analyzed = ""
+        for char in atext:
+            if char != ' ':
+                analyzed+=char
+        atext= analyzed 
+    
+    if flag==1:
+        params={'purpose':message,'analyzed_text':atext}
+        return render(request,'analyze.html',params)
     else:
-        return HttpResponse("Error")
+        return HttpResponse('<h1>Error!!! You have not checked any options </h1>')
 '''def capfirst(request):
     return HttpResponse("Capitalize first")
 
